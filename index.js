eb1798721412529ea27d0f0f6dafd46a32b4ce02
erv <ervinmitra@gmail.com>
2019-07-23 08:49:23 -0700

    scripts file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const flatten = require('./src/flatten');
 const ownLoop = require('./src/ownLoop');
 const everything = require('./src/everything');
+const scripts = require('./src/scripts');
 
 const matrix = [[1, 2, 3], [4, 5], [6]];
 console.log(flatten.flatten(matrix));
@@ -8,3 +9,5 @@ console.log(flatten.flatten(matrix));
 ownLoop.ownLoop(3, n => n > 0, n => n - 1, console.log);
 
 console.log(everything.every([2, 4, 16], n => n < 10));
+
+console.log(scripts[0]);
